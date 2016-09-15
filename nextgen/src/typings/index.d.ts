1f1fd07a3013991df6cdf30b534e8368dab353c0
Delian Delchev <delian.delchev@gmail.com>
2016-09-15 16:16:38 +0200

    Promises added into the typings
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- nextgen/src/typings/index.d.ts
+++ nextgen/src/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/cryptojs/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node-4/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />

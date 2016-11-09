7d5a0569815e65a45d96311086fb65c781815c90
Delian Delchev <delian.delchev@gmail.com>
2016-11-09 13:45:24 +0100

    Experiments with layout
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- app2/cryptarsi/src/app/app.module.ts
+++ app2/cryptarsi/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { MaterialModule } from '@angular/material';
-
 import { AppComponent } from './app.component';
 
 @NgModule({

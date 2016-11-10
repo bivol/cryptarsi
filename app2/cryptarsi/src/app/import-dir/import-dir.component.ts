c2f3857840907d8a5106343883fc3ef26408d908
Delian Delchev <delian.delchev@gmail.com>
2016-11-10 22:30:38 +0100

    Updates

--- app2/cryptarsi/src/app/import-dir/import-dir.component.ts
+++ app2/cryptarsi/src/app/import-dir/import-dir.component.ts
@@ -2,28 +2,70 @@ import { Component, ViewChild } from '@angular/core';
 import { MdInput } from '@angular/material';
 
 @Component({
-    //moduleId: module.id,
+    // moduleId: module.id,
     selector: 'import-dir',
     templateUrl: 'import-dir.component.html',
     styleUrls: ['import-dir.component.css']
 })
 export class ImportDirComponent {
     @ViewChild('dbName') dbName: MdInput;
-    encryptionMenuVisible = true;
+    @ViewChild('encKey1') encKey1: MdInput;
+    @ViewChild('encKey2') encKey2: MdInput;
+    @ViewChild('dirName') dirName;
+
+    encryptionMenuVisible: boolean = true;
+
+    constructor() {
+    }
+
     startEncryption() {
         this.encryptionMenuVisible = false;
         console.log('aaa');
     }
 
-    checkForm(): boolean {
-        console.log('value', this.dbName.value);
+    validateDbName(): boolean {
         if (!this.dbName.value) {
-            this.dbName.focus();
+            this.dbName.dividerColor = 'warn';
+            this.dbName.hintLabel = 'Enter valid name!';
+            return false;
         }
+        this.dbName.dividerColor = 'primary';
+        this.dbName.hintLabel = '';
+        return true;
+    }
+
+    validateKeys(): boolean {
+        if (this.encKey1.value !== this.encKey2.value) {
+            this.encKey1.dividerColor = 'warn';
+            this.encKey1.hintLabel = 'Non equal keys!';
+            this.encKey2.dividerColor = 'warn';
+            this.encKey2.hintLabel = 'Non equal keys!';
+            return false;
+        }
+
+        this.encKey1.hintLabel = '';
+        this.encKey2.hintLabel = '';
+        this.encKey1.dividerColor = 'primary';
+        this.encKey2.dividerColor = 'primary';
+        return true;
+    }
+
+    checkForm(): boolean {
+        if (!this.validateDbName()) {
+            this.dbName.focus();
+            return false;
+        }
+
+        if (!this.validateKeys()) {
+            this.encKey1.focus();
+            return false;
+        }
+
         return true;
     }
 
     submit() {
         this.checkForm();
+        console.log('dir', this.dirName);
     }
 }

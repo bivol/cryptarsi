f1973196707e8dd246bbb0729641d6d480665725
Delian Delchev <delian.delchev@gmail.com>
2016-11-22 20:05:59 +0100

    Search page component
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

---
+++ app2/cryptarsi/src/app/app-search-page/app-search-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    //moduleId: module.id,
+    selector: 'app-search-page',
+    templateUrl: 'app-search-page.component.html',
+    styleUrls: ['app-search-page.component.css']
+})
+export class AppSearchPageComponent {
+
+}

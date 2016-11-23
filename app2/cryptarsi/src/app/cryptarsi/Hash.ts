8537c5790c2f9a43b9d120a306862c3c7ebc7ed0
Delian Delchev <delian.delchev@gmail.com>
2016-11-23 16:49:19 +0100

    fix the regex
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- app2/cryptarsi/src/app/cryptarsi/Hash.ts
+++ app2/cryptarsi/src/app/cryptarsi/Hash.ts
@@ -1,5 +1,5 @@
 export class WordHash {
-    static regex = /^[^\s^.^\-]{2,15}/; // TODO: the regex must be more correct
+    static regex = /^[^\x20-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]{2,15}/; // TODO: the regex must be more correct
 
     static hash(word: string) {
        return word.toLowerCase().match(WordHash.regex)[0]; // TODO: verify this

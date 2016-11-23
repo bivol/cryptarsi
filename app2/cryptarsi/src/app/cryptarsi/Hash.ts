c0aa3e954fe7cefc99c296d1cbefdb64f9eef316
Delian Delchev <delian.delchev@gmail.com>
2016-11-23 15:49:18 +0100

    small change in the regex
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- app2/cryptarsi/src/app/cryptarsi/Hash.ts
+++ app2/cryptarsi/src/app/cryptarsi/Hash.ts
@@ -1,5 +1,5 @@
 export class WordHash {
-    static regex = /^.{2,15}/; // TODO: the regex must be more correct
+    static regex = /^[^\s]{2,15}/; // TODO: the regex must be more correct
 
     static hash(word: string) {
        return word.toLowerCase().match(WordHash.regex)[0]; // TODO: verify this

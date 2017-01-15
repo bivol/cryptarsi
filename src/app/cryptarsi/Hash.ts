3deab2be52d9aebf17454f0298e095dc5a914659
Delian Delchev <delian.delchev@gmail.com>
2017-01-15 15:20:20 +0100

    Hash has been modified to produce fixed length hash with letters A-Z which is later to be encrypted
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/app/cryptarsi/Hash.ts
+++ src/app/cryptarsi/Hash.ts
@@ -5,11 +5,22 @@ export class WordHash {
 
     // a-t-il déclaré à la
     static hash(word: string) {
+        let a = [0, 0, 0, 0, 0, 0, 0, 0];
         if (word) {
             let m = word.toLowerCase().match(WordHash.regex);
-            if (m) { return m[0]; }
+            if (m) {
+                let reg = 0;
+                let w = m[0];
+                for (let pos = 7; pos >= 0; pos--) {
+                    reg += w.charCodeAt(pos % w.length);
+                    reg += w.charCodeAt((8 + pos) % w.length) + 16;
+                    //console.log('reg', reg);
+                    a[pos] += reg;
+                    a[pos] = a[pos] % 27;
+                }
+            }
         }
-        return '';
+        return a.map(c => String.fromCharCode(65 + c)).join('');
     }
 
     static cbPerHash(text: string, cb: any) {

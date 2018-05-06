4f195f443cbd253cb01c1207815a327f7188943a
Delian Delchev <delian.delchev@gmail.com>
2018-05-06 16:50:07 +0200

    PDF and Word
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/app/cryptarsi/IsIndexable.ts
+++ src/app/cryptarsi/IsIndexable.ts
@@ -3,7 +3,10 @@ import { WordHash } from './Hash';
 let indexableList = [
     'text/plain',
     'text/xml',
-    'text/html'
+    'text/html',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 ];
 
 export function isIndexable(type) {
